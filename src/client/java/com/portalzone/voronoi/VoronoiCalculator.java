60e2ca7f53d966f033dc4e0b3856282a6d5d2860
Niall Quinlan <niallquinlan@gmail.com>
2026-01-10 13:13:30 +0000

    changed lod consts

--- src/client/java/com/portalzone/voronoi/VoronoiCalculator.java
+++ src/client/java/com/portalzone/voronoi/VoronoiCalculator.java
@@ -30,8 +30,8 @@ public class VoronoiCalculator {
 
     // LOD (Level of Detail) constants
     private static final int MAX_BORDER_DISTANCE = 2048; // Increase to render farther borders.
-    private static final int[] BASE_LOD_RADII = new int[] {256, 512, 700};
-    private static final int[] LOD_SPACING = new int[] {1, 4, 16, 64};
+    private static final int[] BASE_LOD_RADII = new int[] {100, 300, 500, 700};
+    private static final int[] LOD_SPACING = new int[] {1, 3, 9, 27, 91};
     private static final int TILE_SIZE = 128;
     private static final int WORLD_MIN_Y = -64;
     private static final int WORLD_MAX_Y = 320;
